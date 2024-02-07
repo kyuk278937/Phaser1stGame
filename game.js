2d76e5d8bd948054b3f91aa973a6f43155dfdd1a
kyuk278937 <158030289+kyuk278937@users.noreply.github.com>
2024-02-07 10:42:16 +0200

    0.5
    Додано гравця

--- game.js
+++ game.js
@@ -40,6 +40,30 @@ function create ()
     platforms.create(600, 400, 'ground');
     platforms.create(50, 250, 'ground');
     platforms.create(750, 220, 'ground');
+
+    player = this.physics.add.sprite(100, 450, 'dude');//Створюємо та налаштовуємо спрайт гравця
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+
+    this.anims.create({//Створюємо та налаштовуємо анімації
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'turn',
+        frames: [ { key: 'dude', frame: 4 } ],
+        frameRate: 20
+    });
+
+    this.anims.create({
+        key: 'right',
+        rames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+    });
 }
 
 function update ()
