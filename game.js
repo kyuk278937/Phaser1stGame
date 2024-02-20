5a7eea43fecaf2e18fb8e56ef570e83699cf072b
kyuk278937 <158030289+kyuk278937@users.noreply.github.com>
2024-02-20 19:08:58 +0200

    0.7
    Додано пересування

--- game.js
+++ game.js
@@ -17,6 +17,7 @@ var config = { //Налаштовуємо сцену
 };
 
 var game = new Phaser.Game(config);
+var player;
 
 function preload () //Завантажуємо графіку для гри
 {
@@ -32,21 +33,31 @@ function preload () //Завантажуємо графіку для гри
 
 function create ()
 {
-    this.add.image(400, 300, 'sky');//Додаемо небо
+    //Додаемо небо
+    this.add.image(400, 300, 'sky');
 
-    platforms = this.physics.add.staticGroup();//Створюемо фізичну групу платформ
+    //Ініціалізуємо курсор Phaser
+    cursors = this.input.keyboard.createCursorKeys();
 
-    platforms.create(400, 568, 'ground').setScale(2).refreshBody();//Створюемо платформи
+    //Створюемо фізичну групу платформ
+    platforms = this.physics.add.staticGroup();
+
+    //Створюемо платформи
+    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
     platforms.create(600, 400, 'ground');
     platforms.create(50, 250, 'ground');
     platforms.create(750, 220, 'ground');
 
-    player = this.physics.add.sprite(100, 450, 'dude');//Створюємо та налаштовуємо спрайт гравця
+    //Створюємо та налаштовуємо спрайт гравця
+    player = this.physics.add.sprite(100, 450, 'dude');
     player.setBounce(0.2);
     player.setCollideWorldBounds(true);
-    player.body.setGravityY(300)//Змінено гравітацію гравця
 
-    this.anims.create({//Створюємо та налаштовуємо анімації
+    //Змінено гравітацію гравця
+    player.body.setGravityY(300)
+
+    //Створюємо та налаштовуємо анімації
+    this.anims.create({
         key: 'left',
         frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
         frameRate: 10,
@@ -61,14 +72,36 @@ function create ()
 
     this.anims.create({
         key: 'right',
-        rames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
         frameRate: 10,
         repeat: -1
     });
 
-    this.physics.add.collider(player, platforms);//Додано колізії між гравцем та платформами
+    //Додано колізії між гравцем та платформами
+    this.physics.add.collider(player, platforms);
 }
 
 function update ()
 {
+    //Керування персонажем
+    if (cursors.left.isDown)
+    {
+        player.setVelocityX(-160);
+        player.anims.play('left', true);
+    }
+    else if (cursors.right.isDown)
+    {
+        player.setVelocityX(160);
+        player.anims.play('right', true);
+    }
+    else
+    {
+        player.setVelocityX(0);
+        player.anims.play('turn');
+    }
+
+    if (cursors.up.isDown && player.body.touching.down)
+    {
+        player.setVelocityY(-330);
+    }
 }
